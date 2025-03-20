4a9f73d8cf2361582c8aa5a2292977ff1ed4d14e
kyannepepper <7pepperklein@gmail.com>
2025-03-20 16:12:26 -0600

    you can sign in

--- static/js/login.js
+++ static/js/login.js
@@ -1,11 +1,28 @@
 (() => {
-  const showPassword = document.getElementById("show_password");
   const passwordInput = document.getElementById("password_input");
 
-  showPassword.addEventListener("click", () => {
-    passwordInput.type =
-      passwordInput.type === "password" ? "text" : "password";
-    showPassword.textContent =
-      passwordInput.type === "password" ? "Show" : "Hide";
-  });
+  // showPassword.addEventListener("click", () => {
+  //   passwordInput.type =
+  //     passwordInput.type === "password" ? "text" : "password";
+  //   // showPassword.textContent =
+  //   //   passwordInput.type === "password" ? "Show" : "Hide";
+  // });
 })();
+
+function showLogin() {
+    document.getElementById('ImLoggingIn').style.display = 'flex';
+    document.getElementById('ImCreatingAnAccount').style.display = 'none';
+    document.getElementById('loginButton').classList.add('sliderButtonActive');
+    document.getElementById('loginButton').classList.remove('sliderButtonInactive');
+    document.getElementById('signinButton').classList.add('sliderButtonInactive');
+    document.getElementById('signinButton').classList.remove('sliderButtonActive');
+}
+
+function showSignup() {
+    document.getElementById('ImLoggingIn').style.display = 'none';
+    document.getElementById('ImCreatingAnAccount').style.display = 'flex';
+    document.getElementById('loginButton').classList.add('sliderButtonInactive');
+    document.getElementById('loginButton').classList.remove('sliderButtonActive');
+    document.getElementById('signinButton').classList.add('sliderButtonActive');
+    document.getElementById('signinButton').classList.remove('sliderButtonInactive');
+}
