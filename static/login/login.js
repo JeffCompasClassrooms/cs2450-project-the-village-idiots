4b8ee38acde6c492ee0edba17fe3b360f0b02ebe
Daniel Werner <d00493805@utahtech.edu>
2025-03-26 23:47:02 -0600

    added toggle to the login page

--- static/login/login.js
+++ static/login/login.js
@@ -1,28 +1,62 @@
 (() => {
   const passwordInput = document.getElementById("password_input");
+  const loginButton = document.getElementById('loginButton');
+  const signinButton = document.getElementById('signinButton'); // added to make sure the slider buttons do not get an outline momentarially 
 
-  // showPassword.addEventListener("click", () => {
-  //   passwordInput.type =
-  //     passwordInput.type === "password" ? "text" : "password";
-  //   // showPassword.textContent =
-  //   //   passwordInput.type === "password" ? "Show" : "Hide";
-  // }); 
+  [loginButton, signinButton].forEach(button => {
+    button.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+    }); button.addEventListener('click', () => {
+      button.blur(); //to remove focus from the button and remove the outline
+    });
+  });
+  document.addEventListener('DOMContentLoaded', () => {
+    const toggleIcon = document.getElementById("passwordToggleIcon");
+    toggleIcon.classList.add('password-closed');
+  });
 })();
 
+//function to toggle the visibility of the password and to toggle the eye icon
+function togglePassword() {
+  var password = document.getElementById("password_input");
+  var new_password = document.getElementById("new_password");
+  var toggleIcons = document.getElementsByClassName("passwordToggleIcon");
+  
+  var activePasswordInput = password.offsetParent !== null ? password : new_password;
+  var activeToggleIcon = activePasswordInput === password ? toggleIcons[0] : toggleIcons[1];
+  
+  if(activePasswordInput.type === "password") {
+    activePasswordInput.type = "text";
+    activeToggleIcon.src = "/static/login/images/eye_open.svg";
+    activeToggleIcon.classList.add('password-closed');
+  } else {
+    activePasswordInput.type = "password";
+    activeToggleIcon.src = "/static/login/images/eye_closed.svg";
+    activeToggleIcon.classList.remove('password-closed');
+  }
+}
+
 function showLogin() {
-    document.getElementById('ImLoggingIn').style.display = 'flex';
-    document.getElementById('ImCreatingAnAccount').style.display = 'none';
-    document.getElementById('loginButton').classList.add('sliderButtonActive');
-    document.getElementById('loginButton').classList.remove('sliderButtonInactive');
-    document.getElementById('signinButton').classList.add('sliderButtonInactive');
-    document.getElementById('signinButton').classList.remove('sliderButtonActive');
+  const loginButton = document.getElementById('loginButton');
+  const signinButton = document.getElementById('signinButton');
+
+  document.getElementById('ImLoggingIn').style.display = 'flex';
+  document.getElementById('ImCreatingAnAccount').style.display = 'none';
+
+  loginButton.classList.add('sliderButtonActive');
+  loginButton.classList.remove('sliderButtonInactive');  
+  signinButton.classList.add('sliderButtonInactive');
+  signinButton.classList.remove('sliderButtonActive');
 }
 
 function showSignup() {
-    document.getElementById('ImLoggingIn').style.display = 'none';
-    document.getElementById('ImCreatingAnAccount').style.display = 'flex';
-    document.getElementById('loginButton').classList.add('sliderButtonInactive');
-    document.getElementById('loginButton').classList.remove('sliderButtonActive');
-    document.getElementById('signinButton').classList.add('sliderButtonActive');
-    document.getElementById('signinButton').classList.remove('sliderButtonInactive');
+  const loginButton = document.getElementById('loginButton');
+  const signinButton = document.getElementById('signinButton');
+
+  document.getElementById('ImLoggingIn').style.display = 'none';
+  document.getElementById('ImCreatingAnAccount').style.display = 'flex';
+  loginButton.classList.add('sliderButtonInactive');
+  loginButton.classList.remove('sliderButtonActive');
+  signinButton.classList.add('sliderButtonActive');
+  signinButton.classList.remove('sliderButtonInactive');
 }
