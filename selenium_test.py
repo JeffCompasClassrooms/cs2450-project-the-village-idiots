74cd42ff9c2e8699c0e5a250fee81ad9f4842776
Crew Bindrup <crewbindrup@gmail.com>
2025-04-03 15:43:50 -0600

    Update test.

--- selenium_test.py
+++ selenium_test.py
@@ -6,9 +6,7 @@ import unittest.mock
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-import tinydb
 
-from db import helpers
 
 YOUFACE_URL = "http://localhost:5000"
 
@@ -21,63 +19,6 @@ class TestLoginPage(unittest.TestCase):
     def tearDown(self):
         pass
 
-
-    # def test_login(self):
-    #     driver = webdriver.Chrome()
-
-    #     # 1. request home page
-    #     driver.get(YOUFACE_URL)
-
-    #     #
-    #     # Test invalid login
-    #     #
-
-    #     # 2. enter username and password
-    #     try:
-    #         username_box = driver.find_element(By.NAME, "username")
-    #     except NoSuchElementException:
-    #         self.fail("Failed to find the username input box.")
-    #     username_box.send_keys("invalid")
-
-    #     try:
-    #         password_box = driver.find_element(By.NAME, "password")
-    #     except NoSuchElementException:
-    #         self.fail("Failed to find the password input box.")
-    #     password_box.send_keys("invalid")
-
-    #     # 3. click login button
-    #     try:
-    #         login_btn = driver.find_element(By.CLASS_NAME, "btn-primary")
-    #     except NoSuchElementException:
-    #         self.fail("Failed to find the login button.")
-    #     login_btn.click()
-
-    #     # test login worked
-    #     self.assertIn("loginscreen", driver.current_url)
-
-
-    #     #
-    #     # test valid login
-    #     #
-
-    #     try:
-    #         username_box = driver.find_element(By.NAME, "username")
-    #     except NoSuchElementException:
-    #         self.fail("Failed to find the username input box.")
-    #     username_box.send_keys(self.username)
-    #     try:
-    #         password_box = driver.find_element(By.NAME, "password")
-    #     except NoSuchElementException:
-    #         self.fail("Failed to find the password input box.")
-    #     password_box.send_keys(self.username)
-    #     try:
-    #         login_btn = driver.find_element(By.CLASS_NAME, "btn-primary")
-    #     except NoSuchElementException:
-    #         self.fail("Failed to find the login button.")
-    #     login_btn.click()
-    #     self.assertNotIn("loginscreen", driver.current_url)
-
-    #     driver.close()
 
     def test_create_user(self):
         driver = webdriver.Chrome()
@@ -99,8 +40,6 @@ class TestLoginPage(unittest.TestCase):
 
         driver.close()
 
-    #def test_delete_user(self):
-        #pass
 
 if __name__ == "__main__":
     unittest.main()
